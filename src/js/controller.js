2c89f57fac3ee398ace300b45fee18a0f53231f4
ayush181000 <ayush181000@gmail.com>
2021-10-04 19:16:59 +0530

    Publisher - Subscriber Pattern for event-listeners

--- src/js/controller.js
+++ src/js/controller.js
@@ -20,10 +20,11 @@ const controlRecipes = async function () {
     // 2. Rendering Recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
