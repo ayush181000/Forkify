2fd15289d6e28dba387f15b98c38fa1aadcd34ee
ayush181000 <ayush181000@gmail.com>
2021-10-04 19:31:52 +0530

    Error handling and success message

--- src/js/controller.js
+++ src/js/controller.js
@@ -21,6 +21,7 @@ const controlRecipes = async function () {
     recipeView.render(model.state.recipe);
   } catch (err) {
     console.log(err);
+    recipeView.renderError();
   }
 };
 
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -4,6 +4,8 @@ import { Fraction } from 'fractional';
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
+  #errorMessage = `We could not find that recipe. Please try another one out !`;
+  #message = '';
 
   render(data) {
     this.#data = data;
@@ -16,15 +18,47 @@ class RecipeView {
     this.#parentElement.innerHTML = '';
   }
 
-  renderSpinner = function () {
-    const markup = `<div class="spinner">
-          <svg>
-            <use href="${icons}#icon-loader"></use>
-          </svg>
-        </div>`;
-    this.#parentElement.innerNTML = '';
+  renderSpinner() {
+    const markup = `
+    <div class="spinner">
+        <svg>
+        <use href="${icons}#icon-loader"></use>
+        </svg>
+    </div>`;
+
+    this.#clear();
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  };
+  }
+
+  renderError(message = this.#errorMessage) {
+    const markup = `
+    <div class="error">
+      <div>
+        <svg>
+          <use href="${icons}#icon-alert-triangle"/>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div>`;
+
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderSuccess(message = this.#message) {
+    const markup = `
+    <div class="error">
+      <div>
+        <svg>
+          <use href="${icons}#icon-smile"/>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div>`;
+
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
