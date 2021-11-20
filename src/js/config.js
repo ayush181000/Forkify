ab91866e55044f10bf74d86ba95b8dbce57f41d9
ayush181000 <ayush181000@gmail.com>
2021-11-20 12:31:21 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -3,7 +3,3 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '73e371db-58d6-4a5d-b011-4e222e59dd44';
 export const MODAL_CLOSE_SEC = 2.5;
-
-//73e371db-58d6-4a5d-b011-4e222e59dd44
-
-//759d54ae-3ff9-4711-97b3-1c72802f5f0a
